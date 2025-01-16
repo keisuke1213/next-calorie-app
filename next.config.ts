6609af91b2cdcd38d41199e162cb2c787e6236cb
keisuke1213 <fushanguifu@gmail.com>
2025-01-16 23:43:28 +0900

    chore:apikeyの変更

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+    GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
   },
 };
 
